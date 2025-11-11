a63b0b031613da7b05752135c675f12ab2128ee3
hugeblank <hugeblank@gmail.com>
2025-11-11 02:58:19 -0800

    Update to 1.21.10

--- src/main/java/com/github/nyuppo/compat/Clicker.java
+++
@@ -1,9 +0,0 @@
-package com.github.nyuppo.compat;
-
-import net.minecraft.client.MinecraftClient;
-
-import java.util.Map;
-
-public interface Clicker {
-    void swap(MinecraftClient client, int from, int to);
-}
